7d6679222b865bcd75f62d74315cf887eae5bb9f
Alex M <miramontron@gmail.com>
2019-12-17 10:16:39 -0600

    commits Miramontes code exercise progress

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 /* your scripts */
+// update navLinks
+const navLinks = Array.from(document.querySelectorAll('.link'));
+
+navLinks.forEach(link => {
+    link.addEventListener('click', event => {
+        const target = link.dataset.target;
+        console.log(target);
+
+        navLinks.forEach(t => t.classList.remove('active'));
+        link.classList.add('active');
+    })
+})
+
+//scroll update url 
+window.addEventListener('load', () => {
+    const headings = document.querySelectorAll('a[name]');
+    
+    document.addEventListener('scroll', (e) => {
+        headings.forEach(ha => {
+            const rect = ha.getBoundingClientRect();
+            if(rect.top > 0 && rect.top < 150) {
+                const location = window.location.toString().split('#')[0];
+                history.replaceState(null, null, location + '#' + ha.name);
+            }
+        });
+    });
+});
